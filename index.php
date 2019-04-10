017edb7c26705a39b8d14ad5493f81187ce8c312
Jonatha Silva <joanthab.silva@gmail.com>
2019-04-10 06:00:12 -0300

    Upando arquivos

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html>
+    <head>
+        <script src="https://www.gstatic.com/firebasejs/5.9.3/firebase.js"></script>
+        <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyD8TcVPAPqSR09KRcLL0yq2vXPWjch-2AU",
+            authDomain: "projetom-1dfbc.firebaseapp.com",
+            databaseURL: "https://projetom-1dfbc.firebaseio.com",
+            projectId: "projetom-1dfbc",
+            storageBucket: "projetom-1dfbc.appspot.com",
+            messagingSenderId: "381891497450"
+        };
+        firebase.initializeApp(config);
+        </script>
+    </head>
+
+    <body>
+        <h2>Login do usuário</h2>
+        
+    
+    </body>
+</html>
